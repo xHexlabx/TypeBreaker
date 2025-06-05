b533951f50c8074e8a7e4f859e67b0b95cb72f4b
xhexlabx <tee.hemjinda.work@gmail.com>
2025-06-06 01:58:52 +0700

    Update run_typebreaker.py

--- scripts/run_typebreaker.py
+++ scripts/run_typebreaker.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# เพิ่ม src directory เข้าไปยัง Python path เพื่อให้ import package typebreaker ได้
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 from typebreaker.app_logic import start_app
 
 if __name__ == "__main__":
